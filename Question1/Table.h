7d95bd01f0249ae8951acdb427df85c8403a4b03
NullCodex <jamesonyu95@gmail.com>
2015-06-19 23:47:50 -0400

    Comments

--- Question1/Table.h
+++ Question1/Table.h
@@ -8,17 +8,17 @@ class Table{
 public:
 	friend std::ostream &operator<< (std::ostream &, const Table &);
 	void placeCard(Card*); //add card to table
-	Card* lastCardPlayed();
-	void clear();
-	std::vector <Card*> cardsOnTable();
+	Card* lastCardPlayed(); // Return a pointer to the last game played
+	void clear(); // Wipes the table
+	std::vector <Card*> cardsOnTable(); // Return the cards on table
 private:
-	std::vector <Card*> cardsOnTable_;
-	std::set<int> clubs_;
-	std::set<int> hearts_;
-	std::set<int> spades_;
-	std::set<int> diamonds_;
+	std::vector <Card*> cardsOnTable_; // Cards on table
+	std::set<int> clubs_;  // Cards of suit clubs that were played
+	std::set<int> hearts_; // Cards of suit hearts that were played
+	std::set<int> spades_; // Cards of suit spades that were played
+	std::set<int> diamonds_; // Cards of suit diamonds that were played
 };
 
-std::ostream &operator<<(std::ostream &, const Table &);
+std::ostream &operator<<(std::ostream &, const Table &); // Overloading the ostream operator
 
 #endif
